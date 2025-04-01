3349d52dc6492b851dd07f5d27e6b3d4fbc435ba
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-01 08:45:49 +0200

    Added description

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -2,6 +2,10 @@ import ladyImage from "/lady.jpg"
 
 import { FaQuoteLeft } from "react-icons/fa"
 
+/**
+ * Testimonial component
+ * @param {Object} props - The component props
+ */
 export default function ({ children }) {
   return (
     <section className="flex w-[75%] min-w-[378px] flex-col items-center gap-12 bg-[#2545B8] px-4 py-16 text-[#FFFFFF] lg:max-h-[400px] lg:max-w-[1358px] lg:flex-row lg:px-16">
