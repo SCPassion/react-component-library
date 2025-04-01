1c35528eec5a9099b9ffac3c6603dc37d32d43c9
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-01 08:04:46 +0200

    Added styling for mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import Badge from "./Components/Badge.jsx"
 import Banner from "./Components/Banner.jsx"
 import Card from "./Components/Card/index.jsx"
-
+import ladyImage from "/lady.jpg"
 import { clsx } from "clsx"
+
+import { FaQuoteLeft } from "react-icons/fa"
 
 export default function App() {
   return (
-    <>
-      <Card>
-        <Card.Title>Easy Deployment</Card.Title>
-        <Card.Content>
-          Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et
-          magna sit morbi lobortis.
-        </Card.Content>
-      </Card>
-    </>
+    <section className="flex w-[80%] flex-col items-center gap-12 bg-[#2545B8] px-4 py-16 text-[#FFFFFF]">
+      <img
+        src={`${ladyImage}`}
+        className="-mt-[55%] h-[206px] w-[376px] rounded-xl object-cover object-[center_30%]"
+      />
+      <div>
+        <FaQuoteLeft size={48} className="mb-6 text-[#FFFFFF40]" />
+        <p className="mb-6 text-2xl leading-8 font-medium">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna
+          nulla vitae laoreet augue. Amet feugiat est integer dolor auctor
+          adipiscing nunc urna, sit.{" "}
+        </p>
+        <p className="text-base font-bold">May Andersons</p>
+        <p className="text-base font-medium">Workcation, CTO</p>
+      </div>
+    </section>
   )
 }
